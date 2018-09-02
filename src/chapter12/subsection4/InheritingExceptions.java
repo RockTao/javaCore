33860feb93c5c0c3f70799e52c8cdd060b3a8dcd
root <root@kali>
2018-09-02 22:31:35 +0800

    第十二章  通过异常处理错误

---
+++ src/chapter12/subsection4/InheritingExceptions.java
@@ -0,0 +1,21 @@
+package chapter12.subsection4;
+
+
+
+
+class SimpleException extends Exception {}
+
+public class InheritingExceptions {
+  public void f() throws SimpleException {
+    System.out.println("Throw SimpleException from f()");
+    throw new SimpleException();
+  }
+  public static void main(String[] args) {
+    InheritingExceptions sed = new InheritingExceptions();
+    try {
+      sed.f();
+    } catch(SimpleException e) {
+      System.out.println("Caught it!");
+    }
+  }
+}
