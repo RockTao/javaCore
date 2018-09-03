14b199f7195d7cbe73462194f7a5b72f2a2bcccf
root <root@kali>
2018-09-03 21:46:27 +0800

    容器深入研究list set map contions等容器

---
+++ src/chapter17/subsection_10/Test.java
@@ -0,0 +1,8 @@
+package chapter17.subsection_10;
+public abstract class Test<C> {
+	String name;
+	public Test(String name) { this.name = name; }
+	// Override this method for different tests.
+	// Returns actual number of repetitions of test.
+	abstract int test(C container, TestParam tp);
+} 
